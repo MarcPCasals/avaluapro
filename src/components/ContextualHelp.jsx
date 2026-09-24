b371dff3131a461f374387891aa09af129a98676
Marc Perez Casals <mperezc@educand.ad>
2026-09-24 19:04:40 +0200

    Fa més subtil l'ajuda contextual

--- src/components/ContextualHelp.jsx
+++ src/components/ContextualHelp.jsx
@@ -22,7 +22,7 @@ export function ContextualHelp({ children, className = '', title }) {
         title={`Informació sobre ${title}`}
         type="button"
       >
-        <Info aria-hidden="true" size={15} strokeWidth={2.5} />
+        <span aria-hidden="true" className="contextual-help-letter">i</span>
       </button>
       {open && (
         <Modal onClose={() => setOpen(false)} size="sm" title={title}>
